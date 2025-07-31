16dbe2bcc9b3379ff199022e6265b0e900da639a
cmadaffer <curtismadaffer@gmail.com>
2025-07-30 21:12:50 -0400

    Create connect-freshbooks.js

---
+++ pages/connect-freshbooks.js
@@ -0,0 +1,10 @@
+export default function ConnectFreshbooks() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h2>Connect to FreshBooks</h2>
+      <a href="/api/auth/freshbooks">
+        <button>Connect My FreshBooks Account</button>
+      </a>
+    </div>
+  );
+}
