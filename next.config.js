9405757f2233f876ac43fcaea89470001d1bb5a3
cmadaffer <curtismadaffer@gmail.com>
2025-08-03 15:49:45 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+  async redirects() {
+    return [
+      { source: '/connect', destination: '/connect-freshbooks', permanent: true },
+      { source: '/connect-freshbooks-2', destination: '/connect-freshbooks', permanent: true },
+    ];
+  },
+};
+module.exports = nextConfig;
